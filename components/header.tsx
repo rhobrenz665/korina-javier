36fbabf32b7e535b6cf2c80785bcb6f27ba005e7
rhobrenz665 <buenavistarhobrenz@gmail.com>
2025-03-08 23:16:58 +0800

    Portfolio init with darkmode using shadcn/tailwind 4

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react'
+import ThemeToggler from './toogler-main'
+
+const Header = () => {
+  return (
+    <header>
+      <ThemeToggler />
+    </header>
+  )
+}
+
+export default Header
